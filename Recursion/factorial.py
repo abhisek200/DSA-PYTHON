7dba8136d36457a08152fef333855f5751894096
abhisek200_ <91110493+abhisek200@users.noreply.github.com>
2022-01-17 23:21:05 +0530

    Delete Recursion directory

--- Recursion/factorial.py
+++
@@ -1,8 +0,0 @@
-import sys
-def factorial(n):
-    assert n>=0 and int(n)==n,'the number must be positive.' #assert true statement, (if not true ~ the number must..)(otherwise case)
-    if n in [0,1]:
-        return 1
-    else:
-        return n*factorial(n-1)
-print(factorial(10))
